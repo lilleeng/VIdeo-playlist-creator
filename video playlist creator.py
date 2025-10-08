5bd9bdadde05e65b8eb77b4a3d1fc1d99209c790
lilleeng <klilleengj@gmail.com>
2025-10-09 01:19:47 +0200

    Coding begun

--- video playlist creator.py
+++ video playlist creator.py
@@ -4,5 +4,27 @@
 # Filename for the image will be "cover."
 # Makes a video with the cover image and the songs
 
-import moviepy
+from moviepy import *
 import numpy as np
+import os
+
+datadir = "C:\\Users\\klill\\Downloads\\Tatsuro Yamashita (For Kjetils eyes only)\\Tatsuro Yamashita (For Kjetils eyes only)\\[1982.01.21] For You (VBR)"
+cover_img = os.path.join(datadir, "Cover.jpg")
+song_name = "01. Sparkle"
+song = os.path.join(datadir, song_name + ".mp3")
+
+audio = AudioFileClip(song)
+
+song_duration = 4*60 + 14   # ideally gotten from audio
+
+video = ImageClip(cover_img, duration=song_duration)
+
+
+video.with_audio(audio)
+
+video.write_videofile("Sparkle.avi", 
+                      fps=30, 
+                      codec='rawvideo',
+                      audio_codec='libmp3lame')
+video.close()
+print("done.")
