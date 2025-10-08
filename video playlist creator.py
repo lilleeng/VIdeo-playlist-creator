03faa1ece9ae203e2a1f1cb9c26c4a841158d4e4
lilleeng <klilleengj@gmail.com>
2025-10-08 23:40:14 +0200

    try to commit python script

---
+++ video playlist creator.py
@@ -0,0 +1,8 @@
+
+# Input album folder containing mp3-files and a cover image.
+# Filename formats for the songs will be "n.s", with n a number and s the name of the song.
+# Filename for the image will be "cover."
+# Makes a video with the cover image and the songs
+
+import moviepy
+import numpy as np
