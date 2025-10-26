74ab82e0897a5c2e8497a0dc88cedeac180a328e
lilleeng <klilleengj@gmail.com>
2025-10-26 17:42:47 +0100

    Spillelister kan nå lages

--- video playlist creator.py
+++ video playlist creator.py
@@ -1,4 +1,3 @@
-
 # Input album folder containing mp3-files and a cover image.
 # Filename formats for the songs will be "n.s", with n a number and s the name of the song.
 # Filename for the image will be "cover."
@@ -8,19 +7,47 @@ from moviepy import *
 import numpy as np
 import os
 
+# Enter album data directory
 datadir = input("Album folder directory:\n")
-# datadir = "C:\\Users\\klill\\Downloads\\Tatsuro Yamashita (For Kjetils eyes only)\\Tatsuro Yamashita (For Kjetils eyes only)\\Tatsuro Yamashita - Rarities (2002) (FLAC)"
+# datadir = "C:\\Users\\klill\\Music\\Tatsuro Yamashita\\Sync of Summer (2023)"
+# datadir = "C:\\Users\\klill\\Music\\Tatsuro Yamashita\\[1984.xx.xx] Come Along II (VBR)"
 
+# Confirm content to be used
+songs, imgs, others = [], [], []
+for file in os.listdir(path=datadir):
+    if file == 'cover.jpg' or file == 'backcover.jpg':
+        imgs.append(file)
+        continue
+    splitted_file_name = file.split('.')
+    try:
+        int(splitted_file_name[0])
+        songs.append(file)
+    except:
+        others.append(file)
+songs.sort()
+print('Images to be used:')
+for img in imgs:
+    print(img)
+print('\nSongs to be used:')
+for song in songs:
+    print(song)
+print('\nFiles to be ignored:')
+for other in others:
+    print(other)
+proceed = input('\nProceed with an empty input: ')
+if proceed != '':
+    exit()
 
+# Enter output directory
+output_path = input('Video playlist output path: (empty is same as input path)\n')
 
-cover_img = os.path.join(datadir, "Coverr.jpg")
-song_name = "01. Sparkle.mp3"
-# song = os.path.join(datadir, song_name + ".mp3")
-
-
-
-video = ImageClip(cover_img, duration=1)
-video.write_videofile("Rarities cover and Sparkle song.mp4", 
-                      fps=1,
-                      audio=song_name)
-video.close()
+# Video processing
+cover_img = os.path.join(datadir, "cover.jpg")
+# backcover_img = os.path.join(datadir, "backcover.jpg")    # ignore for now
+for file in songs:
+    song_name, file_ext = file.rsplit('.', maxsplit=1)
+    video = ImageClip(cover_img, duration=1)
+    video.write_videofile(os.path.join(output_path, song_name + '.mp4'),
+                          fps=1,
+                          audio=os.path.join(datadir, file))
+    video.close()
